20365fb5bedd4ca160e1d933722c304d4c85a37d
quarterback <contact@ronbronson.com>
2025-03-29 20:03:58 -0700

    Create Footer.jsx

---
+++ FILC/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+// src/components/Footer.jsx
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-sm text-center py-4 mt-12 border-t">
+      <p>&copy; 2025 Fédération Internationale de Lancer Ciblé. All rights reserved.</p>
+    </footer>
+  );
+}
